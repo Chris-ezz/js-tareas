847eb0dc427b532c5bd67fd61b3200dd8e0f4828
Cristian Alvarez <alvarezcristian21@gmail.com>
2025-02-02 23:03:36 -0300

    Ordenando cada ejercicio con sus archivos

--- clase-6/ejercicio1/main.js
+++ clase-6/ejercicio1/main.js
@@ -110,3 +110,37 @@ function mostrarBorrarIntegrantes() {
 function ocultarBorrarIntegrantes() {
     document.querySelector('#resetear').hidden = true;
 }
+
+function obtenerEdadMayor(edades) {
+    let mayorEdad = edades[0];
+
+    for (let i = 0; i < edades.length; i++) {
+        if (edades[i] > mayorEdad) {
+            mayorEdad = edades[i];
+        }
+    }
+
+    return mayorEdad;
+}
+
+function obtenerEdadMenor(edades) {
+    let menorEdad = edades[0];
+
+    for (let i = 0; i < edades.length; i++) {
+        if (edades[i] < menorEdad) {
+            menorEdad = edades[i];
+        }
+    }
+
+    return menorEdad;
+}
+
+function obtenerEdadPromedio(edades) {
+    let totalEdad = 0;
+
+    for (let i = 0; i < edades.length; i++) {
+        totalEdad += edades[i]; 
+    }
+
+    return totalEdad / edades.length;
+}

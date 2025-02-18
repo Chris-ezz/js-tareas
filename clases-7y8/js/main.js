f494ccf24275b1996e9c3eb126f23a3c0e250b96
Cristian Alvarez <alvarezcristian21@gmail.com>
2025-02-17 23:32:47 -0300

    Modificando archivos
    Refactorizando main.js y agregando validaciones con sus respectivas pruebas.
    Y agregando una mejor descripcion en tarea-clase-8.js

--- clases-7y8/js/main.js
+++ clases-7y8/js/main.js
@@ -1,36 +1,23 @@
+// 1.Obtener el valor de un formulario
 const $form = document.querySelector('#carta-a-santa');
 
-//  id="carta-a-santa".name="nombre".valor
-const nombre = $form.nombre.value;
-//                 name="formulario".name="nombre".y su valor
-//const nombre = document.formulario.nombre.value;
-
-//const ciudad = $form.ciudad.value;
-const ciudad = document.formulario.ciudad.value;
-
-//const comportamiento = $form.comportamiento.value;
-const comportamiento = document.formulario.comportamiento.value;
-
-//Aqui usamos brackets notation para el caso especial del name="descripcion-regalo"
-//const descripcionRegalo = $form['descripcion-regalo'].value;
-const descripcionRegalo = document.formulario['descripcion-regalo'].value;
-
-//console.log(nombre);
-//console.log(ciudad);
-//console.log(comportamiento);
-//console.log(descripcionRegalo);
-
+// 2. Validar que el nombre sea válido y escribir una prueba
 function validarNombre(nombre) {
     if (nombre.length === 0) {
         return 'Este campo debe tener al menos 1 caracter';
     }
+    
     if (nombre.length >= 50) {
         return 'Este campo debe tener menos de 50 caracteres';
     }
 
+    if (!/^[a-z]+$/i.test(nombre)) {
+        return "El campo nombre sólo acepta letras";
+    }
     return "";
 }
 
+// 6.Hacer validación para otros campos (con sus pruebas)
 function validarCiudad(ciudad) {
     if(ciudad === '') {
         return 'Este campo no debe quedar vacío';
@@ -41,11 +28,86 @@ function validarCiudad(ciudad) {
 
 function validarDescripcionRegalo(descripcion) {
     if (descripcion === '') {
-        return 'Este campo no debe quedar vacío';
-    }
-    if(descripcion.length >= 100) {
+        return 'Este campo debe tener por lo menos una letra o número';
+    } else if (descripcion.length >= 100) {
         return 'Este campo debe tener menos de 100 caracteres';
-    }
+    } else if (!/^[a-z0-9 ]+$/i.test(descripcion)) {
+        return "El campo descripción solo puede tener números y letras";
+    } else {
+        return "";
+    } 
+}
+
+// 3. Enviando (submit) el formulario
+function validarFormulario(event) {
+// 4. Eventos. Botón submit
+    // 1.Obtener el valor de un formulario
+    const $form = document.querySelector('#carta-a-santa');
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    //brackets notation para el caso especial de name="descripcion-regalo"
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
     
-    return '';
+    // 7.Objeto
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        'descripcion-regalo': errorDescripcionRegalo
+    };
+    
+    // 9.Interactuando cuando el envío es exitoso
+    const esExito = manejarErrores(errores) === 0;
+    if(esExito) {
+        $form.className = 'oculto';
+        document.querySelector('#exito').className = '';
+        setTimeout(() => {
+            window.location.href = 'wishlist.html';
+        }, 5000);
+    }
+
+    event.preventDefault();
+}
+
+// 5.Éxito o Errores
+function manejarErrores(errores) {
+    borrarErroresAnteriores();
+
+    const keys = Object.keys(errores);
+    let cantidadErrores = 0;
+
+    // 7.forEach y funciones de callback.
+    keys.forEach(function(key){
+        const error = errores[key];
+
+        if (error) {
+            cantidadErrores++;
+            $form[key].className = "error";
+            mostrarErrores(error);
+        } else {
+            $form[key].className = "";
+            //document.querySelector('#errores').className = "oculto"; no es así
+        }
+    });
+    
+    return cantidadErrores;
+}
+
+//3. Enviando (submit) el formulario
+$form.onsubmit = validarFormulario;
+
+function mostrarErrores(error) {
+    const $errores = document.querySelector('#errores');
+
+    const $error = document.createElement('li');
+    $error.textContent = error;
+    $errores.appendChild($error);
+}
+
+function borrarErroresAnteriores() {
+    document.querySelector('#errores').textContent = "";
 }
